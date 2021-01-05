d2f93457d38a745603ef3da75c3fe0d4af8e6163
ally-sorloth <allysorloth@gmail.com>
2021-01-05 23:15:38 +0300

    create detail, uptade, delete files

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,27 +1,62 @@
-from django.shortcuts import redirect, render
-
-from blog.models import Post
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Post
 from .forms import PostForm
 
+
 def post_list(request):
-    qs = Post.objects.all()
+    qs = Post.objects.filter(status='p')
     context = {
-        "object_list":qs
+        "object_list": qs
     }
-    return render(request, 'blog/post_list.html', context)
+    return render(request, "blog/post_list.html", context)
+
 
 def post_create(request):
     # form = PostForm(request.POST or None, request.FILES or None)
     form = PostForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
+            # form.save()
             return redirect("blog:list")
     context = {
         'form': form
     }
     return render(request, "blog/post_create.html", context)
-# Create your views here.
+
+
+def post_detail(request, slug):
+    # Post.objects.get(slug=learn-drf-3c78be2186)
+    obj = get_object_or_404(Post, slug=slug)  # slug = learn-drf-3c78be2186
+    context = {
+        "object": obj
+    }
+    return render(request, "blog/post_detail.html", context)
+
+
+def post_update(request, slug):
+    obj = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=obj)
+    if form.is_valid():
+        form.save()
+        return redirect("blog:list")
+
+    context = {
+        "object": obj,
+        "form": form
+    }
+    return render(request, "blog/post_update.html", context)
+
+
+def post_delete(request, slug):
+    obj = get_object_or_404(Post, slug=slug)
+    if request.method == "POST":
+        obj.delete()
+        return redirect("blog:list")
+    context = {
+        "object": obj
+    }
+    return render(request, "blog/post_delete.html", context)
