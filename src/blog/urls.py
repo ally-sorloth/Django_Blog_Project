a442eb0b3f75286748148245bfbb9539ae93526e
ally-sorloth <allysorloth@gmail.com>
2021-01-07 22:15:02 +0300

    add  detai like

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,6 +1,6 @@
 from os import name
 from django.urls import path
-from .views import post_list, post_create, post_detail, post_update, post_delete
+from .views import post_list, post_create, post_detail, post_update, post_delete, like
 
 
 app_name = "blog"
@@ -10,4 +10,5 @@ urlpatterns = [
     path("<str:slug>/", post_detail, name="detail"),
     path("<str:slug>/update/", post_update, name="update"),
     path("<str:slug>/delete/", post_delete, name="delete"),
+    path("<str:slug>/like/", post_detail, name="delete"),
 ]
